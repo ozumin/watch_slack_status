cd934e33b457cdc4bf50e42bd3bc77db3e228ca2
Mizuo Nagayama <mizuo.n@gmail.com>
2020-03-11 16:18:16 +0900

    change argument name

--- get_slack_status.py
+++ get_slack_status.py
@@ -8,9 +8,9 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 
 # TOKEN取得
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), '/home/lab/mizuo/watch_slack_status/.env')
 load_dotenv(dotenv_path)
-PATH = os.environ.get("PATH") # cron用に絶対パスにするため、リポジトリのパスを読み込む
+DIRPATH = os.environ.get("DIRPATH") # cron用に絶対パスにするため、リポジトリのパスを読み込む
 TOKEN = os.environ.get("TOKEN") # 環境変数の値をAPに代入
 CHANNEL_ID = os.environ.get("CHANNEL_ID") # 送りたいslackのチャンネルID
 MY_NAME = os.environ.get("MY_NAME") # 自分のステータスは通知しない
@@ -18,8 +18,8 @@ client = slack.WebClient(token=TOKEN)
 
 # 前回の結果の読み取り
 last_statuses = ''
-if os.path.exists(PATH + "statuses.pickle"):
-    with open(PATH + 'statuses.pickle', 'rb') as f:
+if os.path.exists(DIRPATH + "statuses.pickle"):
+    with open(DIRPATH + 'statuses.pickle', 'rb') as f:
         last_statuses = pickle.load(f)
 
 # slack API叩く
@@ -32,7 +32,7 @@ for member in member_list:
     statuses[member['name']] = member['profile']['status_text']
 
 # 結果の保存
-with open(PATH + 'statuses.pickle', 'wb') as f:
+with open(DIRPATH + 'statuses.pickle', 'wb') as f:
     pickle.dump(statuses, f)
 
 # 前回との差分をslackに送る
