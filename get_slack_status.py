da1e5076693d1936b69dd28931b82155d9801e1c
Mizuo Nagayama <mizuo.n@gmail.com>
2020-03-11 15:38:48 +0900

    relative to absolute path

--- get_slack_status.py
+++ get_slack_status.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 # TOKEN取得
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+PATH = os.environ.get("PATH") # cron用に絶対パスにするため、リポジトリのパスを読み込む
 TOKEN = os.environ.get("TOKEN") # 環境変数の値をAPに代入
 CHANNEL_ID = os.environ.get("CHANNEL_ID") # 送りたいslackのチャンネルID
 MY_NAME = os.environ.get("MY_NAME") # 自分のステータスは通知しない
@@ -17,8 +18,8 @@ client = slack.WebClient(token=TOKEN)
 
 # 前回の結果の読み取り
 last_statuses = ''
-if os.path.exists("statuses.pickle"):
-    with open('statuses.pickle', 'rb') as f:
+if os.path.exists(PATH + "statuses.pickle"):
+    with open(PATH + 'statuses.pickle', 'rb') as f:
         last_statuses = pickle.load(f)
 
 # slack API叩く
@@ -31,7 +32,7 @@ for member in member_list:
     statuses[member['name']] = member['profile']['status_text']
 
 # 結果の保存
-with open('statuses.pickle', 'wb') as f:
+with open(PATH + 'statuses.pickle', 'wb') as f:
     pickle.dump(statuses, f)
 
 # 前回との差分をslackに送る
